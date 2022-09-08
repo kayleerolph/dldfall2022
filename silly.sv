d529858b8f230e1efa3a1ed3ae2c254cfce7d97a
Brennst <112587024+Brennst@users.noreply.github.com>
2022-09-08 15:57:24 -0500

    Update silly.sv

--- silly.sv
+++ silly.sv
@@ -1,6 +1,6 @@
 module silly (input  logic a, b, c, output logic sum, Cout);
    
-  assign sum = a^b^c;
-  assign Cout = a&b|a&c|b&c;
+  assign sum = a^b^c;  // sets output sum to  a XOR b XOR c
+   assign Cout = a&b|a&c|b&c; // sets output Cout to (a AND b) OR (a AND c) OR (b AND c)
    
 endmodule
